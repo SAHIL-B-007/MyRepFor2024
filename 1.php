78c461683c815a68af6665cecb709e09f05a4554
SAHIL-B-007 <114509855+SAHIL-B-007@users.noreply.github.com>
2024-10-01 11:51:29 +0530

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP PROGRAMS</title>
+</head>
+<body>
+<?php
+	
+	$a=10;
+
+	if($a%2=0)
+		echo "Even No.";
+	else
+		echo "Odd No.";
+
+?>
+</body>
+</html>
